5f6fadbc32c4a6b3ab5357601a0795bb6df6f7e0
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2022-07-26 06:56:26 -0300

    primeiro commit

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world React</h1>
+      <h2>Emmet funcionou</h2>
+      <p>React é muito bom!</p>
+    </div>
+  );
+}
+
+export default App;
