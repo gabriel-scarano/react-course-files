5ef65fca4508a211232b5667ebe521082f8e1a53
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2022-11-05 09:43:04 -0300

    Template Expressions

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,16 @@
-const FirstComponent = () => {
+// arquivo de estilo
 
+const FirstComponent = () => {
+    // essa função faz isso
+
+    /*
+        multi-line
+    */
     return (
         <div>
+            {/* Algum comentário */}
             <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
         </div>
     )
 
