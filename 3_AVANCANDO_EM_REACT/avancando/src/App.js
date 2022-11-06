a57b91beeeba2a4e45ab5aa13a22f90d72c13134
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2022-11-06 09:37:16 -0300

    Trabalhando com imagens

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+import City from './assets/city.jpg';
+
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React</h1>
+      {/* Imagens em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em asset */}
+      <div>
+        <img src={City} alt="Paisagem" />
+      </div>
     </div>
   );
 }
